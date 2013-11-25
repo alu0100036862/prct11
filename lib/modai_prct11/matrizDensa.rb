6def2cdae166d56083d5f188e6f497d87d06aaab
Mohammed Mahrach <alu0100777475@ull.edu.es>
2013-11-25 15:42:30 +0000

    reestructurando jerarquia de clases

---
+++ lib/modai_prct11/matrizDensa.rb
@@ -0,0 +1,164 @@
+# Clase de Matriz densa
+class MatrizDensa < MatrizAbstracta
+
+	# Inicialización
+	def initialize(matriz)
+		super
+	end
+
+	attr_reader :matriz, :filas, :columnas
+
+	# Convertimos a string
+	def to_s
+
+		fil = 0
+		print "["
+		while fil < filas
+
+			col = 0					
+			while col < columnas
+
+				print "#{matriz[fil][col].to_s}"
+				if (col + 1) < columnas then print ", " end
+				col += 1
+
+			end
+
+			if (fil + 1) < filas then print ", " end
+			fil += 1
+
+		end
+		print "]"
+
+	end
+
+	#  Matriz en punto flotante
+	def to_f
+
+                flotante = Array.new(matriz.size - 1)
+                for i in 0...matriz.size
+                        flotante[i] = Array.new(matriz[i].size - 1)
+                        for j in 0...matriz[i].size
+                                flotante[i][j] = (matriz[i][j]).to_f
+                        end
+                end
+                MatrizDensa.new(flotante)
+
+	end
+
+	# Suma de matrices
+	def +(o)
+
+		suma = Array.new(matriz.size - 1)
+#		for i in 0...matriz.size
+		0.upto(matriz.size - 1) do |i|
+		   	suma[i] = Array.new(matriz[i].size - 1)
+#			for j in 0...matriz[i].size
+			0.upto(matriz[i].size - 1) do |j|
+				suma[i][j] = matriz[i][j] + o.matriz[i][j]
+			end
+		end
+		MatrizDensa.new(suma)
+
+	end
+
+        # Suma de matrices densa con dispersa (sobreescribimos el operador / como prueba)
+        def /(o)
+
+                suma = Array.new(matriz.size - 1)
+#		for i in 0...matriz.size
+		0.upto(matriz.size - 1) do |i|
+                        suma[i] = Array.new(matriz[i].size - 1)
+#			for j in 0...matriz[i].size
+			0.upto(matriz[i].size - 1) do |j|
+			
+				suma[i][j] = matriz[i][j]
+					
+				# comprobamos el hash
+	                        if (o.matriz[i] != nil)
+					
+					# hay datos en el has para la columna
+					if o.matriz[i].has_key?(j)						
+                                		suma[i][j] = matriz[i][j] + o.matriz[i][j]
+					end
+					
+				end
+
+                        end
+                end
+                MatrizDensa.new(suma)
+
+        end
+
+
+	# Resta de matrices
+	def -(o)
+
+                resta = Array.new(matriz.size - 1)
+#		for i in 0...matriz.size
+		0.upto(matriz.size - 1) do |i|
+			resta[i] = Array.new(matriz[i].size - 1)
+#			for j in 0...matriz[i].size
+			0.upto(matriz[i].size - 1) do |j|
+				resta[i][j] = matriz[i][j] - o.matriz[i][j]
+			end
+                end
+                MatrizDensa.new(resta)
+
+	end
+
+	# Multiplicación de matrices
+	def *(o)
+
+		prod = Array.new(matriz.size - 1,0)
+#		for i in 0...matriz[0].size 
+		0.upto(matriz[0].size - 1) do |i|
+			prod[i] = Array.new(o.matriz.size,0)
+#			for j in 0...o.matriz.size
+			0.upto(o.matriz.size - 1) do |j|
+#				for pos in 0...matriz.size
+				0.upto(matriz.size - 1) do |pos|
+					prod[i][j] = prod[i][j] + (matriz[i][pos] * o.matriz[pos][j])
+				end
+			end
+		end
+		MatrizDensa.new(prod)
+
+	end
+
+	# Máximo de matriz
+	def max
+
+		maximo = 0.to_f
+		for i in 0...matriz.size
+			for j in 0...matriz[i].size
+				if matriz[i][j].to_f > maximo
+					maximo = matriz[i][j].to_f
+				end
+			end
+		end
+		maximo
+
+	end
+
+	# Minimo de matriz
+	def min
+
+		minimo = $tope
+		for i in 0...matriz.size
+			for j in 0...matriz[i].size
+				if matriz[i][j].to_f < minimo
+					minimo = matriz[i][j].to_f
+				end
+			end
+		end
+		minimo
+
+	end
+
+	# Añadimos el coerce
+	def coerce(other)
+		return self, other
+ 	end
+
+end
---
+++ lib/modai_prct11/racional.rb
@@ -0,0 +1,119 @@
+# racional.rb
+require "./lib/modai_prct11/gcd.rb"
+
+class Racional
+        
+  include Comparable
+        
+  # Se inicializa la fracción en su forma reducida haciendo uso de gcd
+  def initialize(num, denom)
+
+    raise TypeError, "El denominador debe ser mayor que cero" unless (denom != 0)
+
+    div = gcd(num, denom)
+    @num = num/div
+    @denom = denom/div
+
+  end
+
+  # Gets y Sets de @num y @denom
+  attr_reader :num, :denom
+
+  #mostar por la consola la fraccion de la forma: a/b
+  def to_s
+    "#{num}/#{denom}"
+  end
+
+  #comparar que dos fracciones son iguales
+  def ==(o)
+    if o.instance_of? Racional
+        @num == o.num && @denom == o.denom
+    else
+        false
+    end
+  end
+
+  # Mostar la fraccion en formato flotante
+  def to_f
+    num.to_f/denom
+  end
+
+  # Calcular el valor absoluto de una fraccion con el metodo abs
+  def abs
+    Racional.new(num.abs, denom.abs)
+  end
+
+  # calcular el reciproco de una fraccion con el metodo reciprocal
+  def reciprocal
+    Racional.new(denom, num)
+  end
+
+  # Calcular el opuesto de una fraccion con -
+  def -@
+    Racional.new(-denom, num)        
+  end
+
+  # suma dos fracciones con + y dar el resultado de forma reducida
+  def +(o)
+    
+    if o.is_a? Integer
+      o = Racional.new(o,1)
+    end
+    Racional.new(num*o.denom + o.num*denom, denom*o.denom)
+
+  end
+
+  # resta dos fracciones con - y dar el resultado de forma reducida
+  def -(o)
+
+    if o.is_a? Integer
+      o = Racional.new(o,1)
+    end
+    Racional.new(num*o.denom - o.num*denom, denom*o.denom)
+
+  end
+
+  #multiplica dos fracciones con * y dar el resultado de forma reducida
+  def *(o)
+
+    if o.is_a? Integer
+      o = Racional.new(o,1)
+    end
+    Racional.new(num*o.num,denom*o.denom)
+
+  end
+
+  # divide dos fracciones con / y dar el resultado de forma reducida
+  def /(o)
+
+    if o.is_a? Integer
+      o = Racional.new(o,1)
+    end
+    Racional.new(num*o.denom,denom*o.num)
+
+  end
+
+  # calcula el resto dos fracciones con % y dar el resultado de forma reducida
+  def %(o)
+
+    if o.is_a? Integer
+      o = Racional.new(o,1)
+    end
+    Racional.new(num%denom, o.num%o.denom)
+
+  end
+
+  # Comparación de mayor, mayor igual, menor, menor igual
+  def <=>(o)
+    return nil unless o.instance_of? Racional
+    (num.to_f / denom) <=> (o.num.to_f / o.denom)
+  end
+
+  # Añadimos el coerce
+  def coerce(other)
+    return self, other
+  end
+
+end
+
+
