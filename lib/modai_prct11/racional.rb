477caaa2e27d33adb5514455377ba8a30d3e9c6b
Dailos Herrera Bencomo <alu0100036862@etsii.ull.es>
2013-11-25 23:45:38 +0000

    arreglo del coerce de racional

--- lib/modai_prct11/racional.rb
+++ lib/modai_prct11/racional.rb
@@ -66,9 +66,9 @@ class Racional
   # resta dos fracciones con - y dar el resultado de forma reducida
   def -(o)
 
-#    if o.is_a? Integer
-#      o = Racional.new(o,1)
-#    end
+    if o.is_a? Integer
+      o = Racional.new(o,1)
+    end
     Racional.new(num*o.denom - o.num*denom, denom*o.denom)
 
   end
@@ -111,7 +111,7 @@ class Racional
 
   # Añadimos el coerce
   def coerce(other)
-    return self, other
+    return Racional.new(other,1), self
   end
 
 end
